b26ee81ef8270f1fc3e98b368762ce18e07a0537
simonmcho <simonmcho@gmail.com>
2017-10-28 18:30:10 -0700

    updating alogrithm

--- DuplicateEliminationWithArray/A00954121__Cho_Assign05/main.cpp
+++ DuplicateEliminationWithArray/A00954121__Cho_Assign05/main.cpp
@@ -1,26 +1,29 @@
 #include <iostream>
-#include <algorithm>
-#include <iterator>
+#include <array>
+#include <iomanip>
 
 using namespace std;
 
 
-bool isUnique(unsigned int, int [], const size_t);//function prototype to determine if number is unique
-void pushToArray(unsigned int, int[]);
+const size_t size = 20;
+const size_t unique = 2;
+
+bool isUnique(unsigned int, const array<int, ::size> &, const size_t);//function prototype to determine if number is unique
+void pushToArray(unsigned int, array<int, ::size> &);
 bool isInRange(int);
-void printAllNumbers(const int[]);
+void printAllNumbers(const array<int, ::size> &);
 
-int main() 
+int main()
 {
-	//array to hold input
-	const size_t size = 20;
-	unsigned int counter = 1;
-	int numbers[size];
-	int userInput;
+	array< int, ::size> numbers;//array to hold unique numbers from user
+	unsigned int counter = 1;//counter
+	int userInput;//userInput automatic variable
+	const unsigned int nonUnique = 0;//automatic variable to pass in if user input is not unique
 
-	//prompt user to enter a number
+
+									 //prompt user to enter a number
 	while (counter <= 20)
-	{	
+	{
 		//Prompt user
 		cout << "\nEnter # " << counter << " : ";
 		//read input
@@ -28,38 +31,66 @@ int main()
 
 		//if user input is out of the 1 - 100 range, print error
 		if (isInRange(userInput))
-		{	
+		{
 			//check if array already has the user input
 			if (isUnique(userInput, numbers, 20))
 			{
 				cout << "The number: " << userInput << " is unique" << endl;
+
+				//user input is unique number, so push that number to array
+				pushToArray(userInput, numbers);
 			}
-			//if within range, put into array
-			pushToArray(userInput, numbers);
+			else
+			{
+				pushToArray(nonUnique, numbers);//push 0 to array, and do not print anything
+			}
 			counter++;
 		}
-		else {
+		else
+		{
 			cout << "The number entered is not in the valid range of 10 to 100" << endl;
+
 		}
 	}
 
 	//once all 20 numbers is in, print only the unique numbers
+	printAllNumbers(numbers);
 
 
 	system("pause");
-	
+
 }//end of main
 
-void printAllNumbers(const int arrayOfNumbers[])
-{	
 
+
+void printAllNumbers(const array<int, ::size> & arrayUsed)
+{
+	cout << "\nThe unique numbers are: \n" << endl;
+
+	unsigned int characterCount = 0;
+
+	for (auto index : arrayUsed)
+	{
+		if (index > 0 && characterCount < 30)
+		{
+			cout << setw(6) << index;
+			characterCount += 6;
+		}
+		else if (index > 0)
+		{
+			cout << '\n' << setw(6) << index;
+			characterCount = 6;
+		}
+	}
+
+	cout << '\n' << endl;
 }
 
-bool isUnique(unsigned int numberToCheck, int arrayOfNumbers[], const size_t sizeOfArray)
+bool isUnique(unsigned int numberToCheck, const array<int, ::size> & arrayToCheck, const size_t sizeOfArray)
 {
 	for (size_t i = 0; i < sizeOfArray; i++)
 	{
-		if (numberToCheck == arrayOfNumbers[i])
+		if (arrayToCheck[i] == numberToCheck)
 		{
 			return false;
 		}
@@ -76,7 +107,7 @@ bool isInRange(int input)
 	return false;
 }
 
-void pushToArray(unsigned int input, int arrayUsed[])
+void pushToArray(unsigned int input, array<int, ::size> & arrayUsed)
 {
 	static size_t arrayIndex = 0;
 
