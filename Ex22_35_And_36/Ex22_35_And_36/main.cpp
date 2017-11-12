dd424cd5447397e8ac83a6ea594c0d6be528b6ee
simonmcho <simonmcho@gmail.com>
2017-11-11 16:54:37 -0800

    finished c sring manipulation practice

--- Ex22_35_And_36/Ex22_35_And_36/main.cpp
+++ Ex22_35_And_36/Ex22_35_And_36/main.cpp
@@ -2,8 +2,6 @@
 //  main.cpp
 //  Ex22_35_And_36
 //  C String manipulations using functions from the <cstring> library
-//  22.35: Allow user to input a telephone number, and output that in a sequential concatenated string,
-//         ignoring all characters other than the numbers 0 - 9.
 //
 //  Created by simmons on 2017-11-10.
 //  Copyright © 2017 simmonscho. All rights reserved.
@@ -13,15 +11,61 @@
 #include <cstring>
 
 using namespace std;
-
+/*
+ 
+// Allow user to input a telephone number, and output that in a sequential concatenated string,
+// ignoring all characters other than the numbers 0 - 9.
 int main(int argc, const char * argv[]) {
     char userPhoneNumber[80];
+    char token[] = "()- ";
+    char parsedPhoneNumber[10];
     
     cout << "Enter your phone number" << endl;
     
     cin.getline(userPhoneNumber, 80);
     
-    cout << strtok(userPhoneNumber, "-") << endl;
+    char *tokenPtr =  strtok(userPhoneNumber, token);
+    
+    
+    while ( tokenPtr != NULL)
+    {
+        strcat(parsedPhoneNumber, tokenPtr);
+        tokenPtr = strtok(NULL, token);
+    }
+    
+    cout << parsedPhoneNumber << endl;
     
     return 0;
+}
+ */
+
+
+// Take a sentence input and tokenize the inputted text and outputs in reverse order
+int main()
+{
+    cout << "Enter a sentence: \n" << endl;
+    
+    char sentence[80];
+    char *pointers[20];
+    unsigned int count = 0;
+    char tokenSeparator[] = " ,.!?;:";
+    
+    cin.getline(sentence, 80);
+    
+    char *tokenPtr = strtok(sentence, tokenSeparator);
+    
+    while(tokenPtr != NULL)
+    {
+        pointers[count++] = tokenPtr;
+        tokenPtr = strtok(NULL, tokenSeparator);
+    }
+    
+    count--;
+    
+    for(int i = count; i >= 0; i--)
+    {
+        cout << pointers[i] << ' ';
+    }
+    cout << endl;
+    //system("pause"); //xcode "pause" cmd not found
 }
